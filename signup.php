04fc4cdb57d7ba59dc63af1130a640541d6cfcb8
Prav-booo <56572515+Prav-booo@users.noreply.github.com>
2019-10-30 14:13:18 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>user login</title>
-	<link href="dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
