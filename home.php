520c81b3467cd883d2e0de3ab61887adeaf8347d
Prav-booo <56572515+Prav-booo@users.noreply.github.com>
2019-10-30 14:18:42 +0530

    Update home.php

--- home.php
+++ home.php
@@ -12,9 +12,9 @@ include 'session.php';
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
     <title>Library </title>
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link href="ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <script src="ie-emulation-modes-warning.js"></script>
     <link href="style.css" rel="stylesheet">
   </head>
   
@@ -36,15 +36,15 @@ include 'session.php';
     
 		<div class="carousel-inner">
 			  <div class="item active">
-				<img src="assets/img/lib.png" alt="libary" style="width:100%;">
+				<img src="lib.png" alt="libary" style="width:100%;">
 			  </div>
 
 			<div class="item">
-				<img src="assets/img/lib1.png" alt="libary" style="width:100%;">
+				<img src="lib1.png" alt="libary" style="width:100%;">
 			</div>
     
 		  <div class="item">
-			<img src="assets/img/lib2.png" alt="libary" style="width:100%;">
+			<img src="lib2.png" alt="libary" style="width:100%;">
 		  </div>
 		</div>
 
@@ -64,7 +64,7 @@ include 'session.php';
 	  <div class="container marketing">
       <div class="row">
         <div class="col-md-12">
-          <img class="img-circle" src="assets/img/img.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="img.png" alt="Generic placeholder image" width="140" height="140">
           <h2>The Handmaid's Tale</h2>
           <p>The Handmaid's Tale is a dystopian novel[2] by Canadian author Margaret Atwood,[3][4] originally published in 1985. It is set in a near-future New England, in a totalitarian state resembling a theonomy that overthrows the United States government.[5] The novel focuses on the journey of the handmaid Offred. Her name derives from the possessive form "of Fred"; handmaids are forbidden to use their birth names and must echo the male, or master, whom they serve.
 
@@ -72,13 +72,13 @@ The Handmaid's Tale explores themes of women in subjugation in a patriarchal soc
           <p><a class="btn btn-default" href="https://en.wikipedia.org/wiki/The_Handmaid%27s_Tale" role="button">View details &raquo;</a></p>
         </div>
         <div class="col-md-4">
-          <img class="img-circle" src="assets/img/img1.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="img1.png" alt="Generic placeholder image" width="140" height="140">
           <h2>Invisible Man</h2>
           <p>Invisible Man is a novel by Ralph Ellison, published by Random House in 1952. It addresses many of the social and intellectual issues facing African Americans early in the twentieth century, including black nationalism, the relationship between black identity and Marxism, and the reformist racial policies of Booker T. Washington, as well as issues of individuality and personal identity.</p>
           <p><a class="btn btn-default" href="https://en.wikipedia.org/wiki/Invisible_Man" role="button">View details &raquo;</a></p>
         </div>
         <div class="col-md-4">
-          <img class="img-circle" src="assets/img/img2.png" alt="Generic placeholder image" width="140" height="140">
+          <img class="img-circle" src="img2.png" alt="Generic placeholder image" width="140" height="140">
           <h2>The Trial</h2>
           <p>The Trial (original German title: Der Process,[1] later Der Proceß, Der Prozeß and Der Prozess) is a novel written by Franz Kafka between 1914 and 1915 and published posthumously in 1925. One of his best-known works, it tells the story of Josef K., a man arrested and prosecuted by a remote, inaccessible authority, with the nature of his crime revealed neither to him nor to the reader. Heavily influenced by Dostoyevsky's Crime and Punishment and The Brothers Karamazov, Kafka even went so far as to call Dostoyevsky a blood relative.[2] Like Kafka's other novels, The Trial was never completed, although it does include a chapter which appears to bring the story to an intentionally abrupt ending.</p>
           <p><a class="btn btn-default" href="https://en.wikipedia.org/wiki/The_Trial" role="button">View details &raquo;</a></p>
@@ -96,7 +96,7 @@ The Handmaid's Tale explores themes of women in subjugation in a patriarchal soc
 			The novel features the character David Copperfield, and is written in the first person, as a description of his life until middle age, with his own adventures and the numerous friends and enemies he meets along his way. It is his journey of change and growth from infancy to maturity, as people enter and leave his life and he passes through the stages of his development.</p>
         </div>
         <div class="col-md-5">
-          <img class="featurette-image img-responsive center-block" src="assets/img/img3.png" alt="Generic placeholder image">
+          <img class="featurette-image img-responsive center-block" src="img3.png" alt="Generic placeholder image">
         </div>
       </div>
 
@@ -108,15 +108,15 @@ The Handmaid's Tale explores themes of women in subjugation in a patriarchal soc
           <p class="lead">The Call of the Wild is a short adventure novel by Jack London, published in 1903 and set in Yukon, Canada, during the 1890s Klondike Gold Rush, when strong sled dogs were in high demand. The central character of the novel is a dog named Buck. The story opens at a ranch in Santa Clara Valley, California, when Buck is stolen from his home and sold into service as a sled dog in Alaska. He becomes progressively feral in the harsh environment, where he is forced to fight to survive and dominate other dogs. By the end, he sheds the veneer of civilization, and relies on primordial instinct and learned experience to emerge as a leader in the wild.</p>
         </div>
         <div class="col-md-5 col-md-pull-7">
-          <img class="featurette-image img-responsive center-block" src="assets/img/img4.png" alt="Generic placeholder image">
+          <img class="featurette-image img-responsive center-block" src="img4.png" alt="Generic placeholder image">
         </div>
       </div>
 	  
 	  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/vendor/holder.min.js"></script>
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script>window.jQuery || document.write('<script src="jquery.min.js"><\/script>')</script>
+    <script src="bootstrap.min.js"></script>
+    <script src="holder.min.js"></script>
+    <script src="ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
